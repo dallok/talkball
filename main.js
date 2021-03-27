58f0c378b8606ebef8572596a64635e4fcc43c30
Sujin <cavok0929@gmail.com>
2021-03-28 02:02:51 +0900

    JS: Darkmode

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function darkmod() {
-  
-}
+const darkModeBtn = document.querySelector(".darkMode");
+const chatContainer = document.querySelector(".chat__container")
+
+darkModeBtn.addEventListener("click", () => {
+  if (chatContainer.classList.contains("dark")) {
+    chatContainer.classList.remove("dark");
+  } else {
+    chatContainer.classList.add("dark");
+  }
+})
