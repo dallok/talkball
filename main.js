b322ed06ed602af99fe8087423bbe78427b12c7a
Sujin <cavok0929@gmail.com>
2021-03-29 18:55:58 +0900

    JS: Toggle button handling for mobile version

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+"use strict";
+
+// Toggle button control for mobile version //
+const navbarMenu = document.querySelector(".menubar")
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
+
+// Chatbox Darkmode Control //
 const darkModeBtn = document.querySelector(".darkMode");
 const chatContainer = document.querySelector(".chat__container")
 
