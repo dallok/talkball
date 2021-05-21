35d3731be9acd32374e55ce8110eee9a576235b5
Sujin <cavok0929@gmail.com>
2021-05-21 11:18:13 +0900

    Random Talk: Modal

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Toggle button control for mobile version //
+// Toggle button control for mobile version 
 const navbarMenu = document.querySelector(".menubar")
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
 navbarToggleBtn.addEventListener("click", () => {
@@ -8,7 +8,7 @@ navbarToggleBtn.addEventListener("click", () => {
 });
 
 
-// Chatbox Darkmode Control //
+// Chatbox Darkmode Control 
 const darkModeBtn = document.querySelector(".darkMode");
 const chatContainer = document.querySelector(".chat__container")
 
@@ -19,3 +19,11 @@ darkModeBtn.addEventListener("click", () => {
     chatContainer.classList.add("dark");
   }
 })
+
+// Random Talk Modal Control
+var myModal = document.getElementById('myModal');
+var myInput = document.getElementById('myInput');
+
+myModal.addEventListener('shown.bs.modal', function () {
+	myInput.focus();
+});
